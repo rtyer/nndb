47557d16063c55d7aac33e0a840406f9169347f3
Ryan Tyer <ryan.tyer@gmail.com>
2016-06-26 15:17:09 -0600

    Updated to use a make target to get the file, not check it in

--- food.go
+++ food.go
@@ -1,17 +1,24 @@
 package nndb
 
-// Food represents a food item, sourced from the Nutrient Databank
+// Models representing data sourced from the National Nutrient Database
+
+// Food represents a food item
 type Food struct {
-	// NBDID is the Nutrient Databank ID
 	ID            int
 	FoodGroup     FoodGroup
 	Name          string
 	AlternateName string
 	Manufacturer  string
+	Nutrients     Nutrients
 }
 
-// FoodGroup represents a food group, sourced from the Nutrient Databank
+// FoodGroup represents a food group, such as `Cereal Grains and Pasta`
 type FoodGroup struct {
 	ID   int
 	Name string
+}
+
+// Nutrients represents the nutritional information, such as `Calories`
+type Nutrients struct {
+	Calories int
 }
