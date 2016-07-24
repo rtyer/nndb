2edafd0cc200b401ea6f5b268ae3a7bd216ef575
Ryan Tyer <ryan.tyer@gmail.com>
2016-07-24 15:07:46 -0600

    beginning to work on the CLI

--- cmd/nndb/main.go
+++ cmd/nndb/main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"fmt"
+	"os"
 
-	"github.com/rtyer/nndb"
+	"github.com/codegangsta/cli"
 )
 
 func main() {
-	n := nndb.Nutrients{
-		Calories: 100,
+	app := cli.NewApp()
+
+	app.Action = func(c *cli.Context) error {
+		fmt.Printf("Hello %q\n", c.Args().Get(0))
+		return nil
 	}
-	fmt.Printf("hi%v\n", n)
+
+	err := app.Run(os.Args)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1001)
+	}
 }
